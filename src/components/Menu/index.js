aa03ce3afbd623c95e4c9e62a0ee77365944bdbc
KAILAS-NATH-K-S <.\.git\kailasnks23@gmail.com>
2024-03-22 13:54:13 +0530

    created menuSlice

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,29 +1,51 @@
+import React from "react";
+import styles from "./index.module.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faPencil,
+  faEraser,
+  faRotateLeft,
+  faRotateRight,
+  faFileArrowDown,
+} from "@fortawesome/free-solid-svg-icons";
+import { useDispatch,useSelector } from "react-redux";
+import { MENU_ITEMS } from "@/constants";
+import { menuItemClick, actionItemClick } from "@/slice/menuSlice";
 
-import React from 'react';
-import styles from './index.module.css'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faPencil, faEraser, faRotateLeft, faRotateRight, faFileArrowDown } from '@fortawesome/free-solid-svg-icons'
-const Menu = () =>{
-     
-    return(
-        <div className={styles.menuContainer} >
-             <div className = {styles.iconWrapper}>
-                <FontAwesomeIcon icon={faPencil} className={styles.icon} />
-            </div>
-            <div className={styles.iconWrapper}>
-                <FontAwesomeIcon icon={faEraser} className={styles.icon} />
-            </div>
-            <div className={styles.iconWrapper}>
-                <FontAwesomeIcon icon={faRotateLeft} className={styles.icon}/>
-            </div>
-            <div className={styles.iconWrapper}>
-                <FontAwesomeIcon icon={faRotateRight} className={styles.icon}/>
-            </div>
-            <div className={styles.iconWrapper}>
-                <FontAwesomeIcon icon={faFileArrowDown} className={styles.icon}/>
-            </div>
-        </div>
-    )
-}
+
+const Menu = () => {
+  const dispatch = useDispatch();
+  const activeMenuItem = useSelector((state) => state.menu.activeMenuItem);
+
+  const handleMenuClick = (itemName) => {
+    dispatch(menuItemClick(itemName));
+  };
+  return (
+    <div className={styles.menuContainer}>
+      <div
+        className={styles.iconWrapper}
+        onClick={() => handleMenuClick(MENU_ITEMS.PENCIL)
+    }
+      >
+        <FontAwesomeIcon icon={faPencil} className={styles.icon} />
+      </div>
+      <div
+        className={styles.iconWrapper}
+        onClick={() => handleMenuClick(MENU_ITEMS.ERASER)}
+      >
+        <FontAwesomeIcon icon={faEraser} className={styles.icon} />
+      </div>
+      <div className={styles.iconWrapper}>
+        <FontAwesomeIcon icon={faRotateLeft} className={styles.icon} />
+      </div>
+      <div className={styles.iconWrapper}>
+        <FontAwesomeIcon icon={faRotateRight} className={styles.icon} />
+      </div>
+      <div className={styles.iconWrapper}>
+        <FontAwesomeIcon icon={faFileArrowDown} className={styles.icon} />
+      </div>
+    </div>
+  );
+};
 
 export default Menu;
