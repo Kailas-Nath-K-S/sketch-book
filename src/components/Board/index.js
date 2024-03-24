455dcfdad7484c42a51a5da94984aec6693a77d1
KAILAS-NATH-K-S <.\.git\kailasnks23@gmail.com>
2024-03-24 16:40:33 +0530

    Created toolboxSlice

--- src/components/Board/index.js
+++ src/components/Board/index.js
@@ -1,8 +1,10 @@
 import { useEffect, useRef } from "react"
-
+import { useDispatch, useSelector } from "react-redux";
 
 const Board = () =>{
  const canvasRef = useRef(null);
+ const activeMenuItem = useSelector((state) => state.menu.activeMenuItem);
+ const{color,size}=useSelector((state) => state.toolbox[activeMenuItem]);
 useEffect(()=>{
     if (!canvasRef.current)return
 const canvas= canvasRef.current;
@@ -11,7 +13,7 @@ const context = canvas.getContext('2d');
 canvas.width=window.innerWidth;
 canvas.height=window.innerHeight;
 },[])
-
+console.log(color,size)
     return(<canvas ref={canvasRef}></canvas>)
 }
 export default Board
--- src/components/Toolbox/index.js
+++ src/components/Toolbox/index.js
@@ -1,13 +1,21 @@
 import { COLORS, MENU_ITEMS } from "@/constants";
 import styles from "./index.module.css";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import {changeColor, changeBrushSize} from "@/slice/toolboxSlice";
 const Toolbox = () => {
+  const dispatch=useDispatch( );
   const activeMenuItem = useSelector((state) => state.menu.activeMenuItem);
   const showStrokeToolOption = activeMenuItem === MENU_ITEMS.PENCIL;
   const showBrushToolOption =
     activeMenuItem === MENU_ITEMS.PENCIL ||
     activeMenuItem === MENU_ITEMS.ERASER;
-  const updateBrushSize = (e) => {};
+  const updateBrushSize = (e) => {
+    dispatch(changeBrushSize({item: activeMenuItem,size:e.target.value}))
+  };
+  const updateColor=(newColor)=>{
+    dispatch(changeColor({item: activeMenuItem,color:newColor}))
+  };
+  
   return (
     <div className={styles.toolboxContainer}>
       {showStrokeToolOption && (
@@ -16,27 +24,27 @@ const Toolbox = () => {
           <div className={styles.itemContainer}>
             <div
               className={styles.colorBox}
-              style={{ backgroundColor: COLORS.BLACK }}
+              style={{ backgroundColor: COLORS.BLACK }} onClick={()=>updateColor(COLORS.BLACK)}
             />
             <div
               className={styles.colorBox}
-              style={{ backgroundColor: COLORS.RED }}
+              style={{ backgroundColor: COLORS.RED }}  onClick={()=>updateColor(COLORS.RED)}
             />
             <div
               className={styles.colorBox}
-              style={{ backgroundColor: COLORS.GREEN }}
+              style={{ backgroundColor: COLORS.GREEN }}  onClick={()=>updateColor(COLORS.GREEN)}
             />
             <div
               className={styles.colorBox}
-              style={{ backgroundColor: COLORS.BLUE }}
+              style={{ backgroundColor: COLORS.BLUE }} onClick={()=>updateColor(COLORS.BLUE)}
             />
             <div
               className={styles.colorBox}
-              style={{ backgroundColor: COLORS.ORANGE }}
+              style={{ backgroundColor: COLORS.ORANGE }} onClick={()=>updateColor(COLORS.ORANGE)}
             />
             <div
               className={styles.colorBox}
-              style={{ backgroundColor: COLORS.YELLOW }}
+              style={{ backgroundColor: COLORS.YELLOW }} onClick={()=>updateColor(COLORS.YELLOW)}
             />
           </div>
         </div>
